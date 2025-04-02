2934a098fab5056a9fdc6df80badc8b14d63b206
Protik Nag <protiknag08@gmail.com>
2025-04-02 10:49:59 -0400

    Minor adjustments

--- config.py
+++ config.py
@@ -9,17 +9,21 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
+CONCEPT_FOLDER = "./data/concept/stripes_fake"
+RANDOM_FOLDER = "./data/concept/random"
+BINARY_CLASSIFICATION_BASE = "./data/binary_classification/"
+RESULTS_PATH = "./results/retrained_model.pth"
+
 LEARNING_RATE = 1e-3
 EPOCHS = 10
 BATCH_SIZE = 64
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 NUM_CLASSES = 2
-MODEL = training_biased_model(DeepCNN(num_classes=NUM_CLASSES).to(DEVICE))
+MODEL = training_biased_model(
+    DeepCNN(num_classes=NUM_CLASSES).to(DEVICE),
+    BINARY_CLASSIFICATION_BASE
+)
 LAYER_NAME = "conv_block4"
-CONCEPT_FOLDER = "./data/concept/stripes_fake"
-RANDOM_FOLDER = "./data/concept/random"
-BINARY_CLASSIFICATION_BASE = "./data/binary_classification/"
-RESULTS_PATH = "./results/retrained_model.pth"
 ZEBRA_CLASS_NAME = "zebra"
 LAMBDA_ALIGN = 0.75
 LAMBDA_CLS = 1.0 - LAMBDA_ALIGN
