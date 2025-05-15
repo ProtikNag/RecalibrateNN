6f4547352aa3d0ca20dcdbdca3a9780a3bc33f7b
Protik Nag <protiknag08@gmail.com>
2025-05-15 01:15:45 -0400

    Changed train.py for three different cases

--- train.py
+++ train.py
@@ -11,20 +11,25 @@ from torchvision import transforms
 from custom_dataloader import MultiClassImageDataset
 from model import DeepCNN
 from utils import get_class_folder_dicts
-from new_config import get_num_classes
 
 SEED = 0
 random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
-NUM_CLASSES = get_num_classes('./data/multi_class_classification/')
+def get_num_classes(base_path):
+    return len([
+        name for name in os.listdir(base_path)
+        if os.path.isdir(os.path.join(base_path, name))
+    ])
+
+NUM_CLASSES = get_num_classes('./data/multi_class_classification_1/')
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def training_biased_model(
         model,
-        data_path="./data/multi_class_classification/",
+        data_path="./data/multi_class_classification_1/",
         max_epochs=50,
         batch_size=32,
         learning_rate=1e-4,
@@ -160,5 +165,13 @@ def training_biased_model(
 
 if __name__ == "__main__":
     model = DeepCNN(NUM_CLASSES).to(DEVICE)
-    trained_model = training_biased_model(model)
-    torch.save(trained_model.state_dict(), "./model_weights/imbalanced_model.pth")
+    trained_model = training_biased_model(model, data_path="./data/multi_class_classification_1/")
+    torch.save(trained_model.state_dict(), "./model_weights/imbalanced_model_1.pth")
+
+    model = DeepCNN(NUM_CLASSES).to(DEVICE)
+    trained_model = training_biased_model(model, data_path="./data/multi_class_classification_2/")
+    torch.save(trained_model.state_dict(), "./model_weights/imbalanced_model_2.pth")
+
+    model = DeepCNN(NUM_CLASSES).to(DEVICE)
+    trained_model = training_biased_model(model, data_path="./data/multi_class_classification_3/")
+    torch.save(trained_model.state_dict(), "./model_weights/imbalanced_model_3.pth")
