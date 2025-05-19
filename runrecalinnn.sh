235c525f022ded5b3619b1b492740ba73a8339fe
srikanthks-hsr <srikanthks.hsr@gmail.com>
2025-05-19 10:26:17 +0530

    refactored to make legacy code with DeepCNN also work

---
+++ runrecalinnn.sh
@@ -0,0 +1,5 @@
+python main.py --model_name vgg16  --model_path /home/srikanth/trained_models/pytorch
+python main.py --model_name resnet50 --model_path /home/srikanth/trained_models/pytorch
+python main.py --model_name inception_v3 --model_path /home/srikanth/trained_models/pytorch
+python main.py --model_name mobilenet_v3_small --model_path /home/srikanth/trained_models/pytorch
+python main.py --model_name mobilenet_v3_large --model_path /home/srikanth/trained_models/pytorch
