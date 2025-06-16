3c1380485938aab9b95f82c66be64f37bbda4e06
srikanthks-hsr <srikanthks.hsr@gmail.com>
2025-06-16 17:39:11 +0530

    fixed minor exceptions

--- main_compute_sensitivity.py
+++ main_compute_sensitivity.py
@@ -88,7 +88,8 @@ def main():
                 independent_sensitivityscore = [util_compute_sensitivity_score(model_trained, layer_name, cav, class_loader, idx, activation) \
                             for cav, class_loader, idx in zip(cav_vectors, class_dataloaders, TARGET_IDX_LIST)]
                             
-                independent_sensitivityscore = np.concatenate(independent_sensitivityscore.cpu())
+                independent_sensitivityscore = [cpudata.cpu().numpy() for cpudata in independent_sensitivityscore]
+                independent_sensitivityscore = np.concatenate(independent_sensitivityscore)
                 sensitivityscore_Before = f"sensitivityscore_before_{layer_name}"
                 df[sensitivityscore_Before ] = independent_sensitivityscore
                 print(df[sensitivityscore_Before])
@@ -176,7 +177,9 @@ def main():
 
                     independent_sensitivityscore_after = [util_compute_sensitivity_score(model_trained, layer_name, cav, class_loader, idx, activation)
                             for cav, class_loader, idx in zip(cav_vectors, class_dataloaders, TARGET_IDX_LIST)]
-                    independent_sensitivityscore_after = np.concatenate(independent_sensitivityscore_after.cpu())
+                            
+                    independent_sensitivityscore_after = [cpudata.cpu().numpy() for cpudata in independent_sensitivityscore_after]
+                    independent_sensitivityscore_after = np.concatenate(independent_sensitivityscore_after)
                     sensitivityscore_After = f"sensitivityscore_After_{layer_name}_{LAMBDA_ALIGN}" 
                     df[sensitivityscore_After] = independent_sensitivityscore_after
                     df.to_csv(dataframe_filename, index = False)
