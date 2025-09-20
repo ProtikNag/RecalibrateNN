7ce5e7f9f425356eef14e98da57d728251829f35
srikanthks.hsr <srikanthks.hsr@gmail.com>
2025-09-20 22:53:32 +0530

    added checks

--- ConfigSingleton.py
+++ ConfigSingleton.py
@@ -43,12 +43,74 @@ class ConfigSingleton:
         self.LEARNING_RATE = float(config['hyperparameters']['learning_rate'])
         self.EPOCHS = int(config['hyperparameters']['epochs'])
         self.BATCH_SIZE = config['hyperparameters']['batch_size']
+        
+        # Catch if the links are missing and raise an exception in case its not found
+        not_found = 0
+        print(self.CONCEPT_FOLDER_LIST)
+        for concept_folder in self.CONCEPT_FOLDER_LIST:
+            results = self._verify_files_links(concept_folder)
+            if(results == -1):
+                print("Folder not found ",concept_folder)
+                notfound = 1
+        print(self.RANDOM_FOLDER)
+        results = self._verify_files_links(self.RANDOM_FOLDER)
+        if(results == -1):
+            print("Folder not found ",random_folder)
+            notfound = 1
+        base_path = self.CLASSIFICATION_DATA_BASE_PATH
+        for target in self.TARGET_CLASS_LIST:
+            folder_path = os.path.join(base_path,"train/"+ target)
+            results = self._verify_files_links(folder_path)
+        base_path = self.CLASSIFICATION_DATA_BASE_PATH
+        for target in self.TARGET_CLASS_LIST:
+            folder_path = os.path.join(base_path,"valid/"+ target)
+            results = self._verify_files_links(folder_path)
+        
+        if(not_found == 1):
+            raise(" Some of the folders are not found or an incorrect link ")
+        
 
     def _get_num_classes(self, base_path):
         return len([
             name for name in os.listdir(base_path)
             if os.path.isdir(os.path.join(base_path, name))
         ])
+        
+    def _verify_files_links(self, base_path):
+        """
+        Checks if the given path is a symbolic link, a regular file, a directory,
+        or if it doesn't exist.
+        """
+        return_value = 0
+        if os.path.islink(base_path):
+            # The path itself is a symbolic link
+            print(f"The path '{base_path}' is a symbolic link.")
+            return_value = 0
+            
+            # You can also check what the link points to
+            if os.path.isdir(base_path):
+                print("  It points to a directory.")
+                return_value = 0
+            elif os.path.isfile(base_path):
+                print("  It points to a file.")
+                return_value = 0
+            else:
+                print("  It is a broken or invalid link (target does not exist).")
+                return_value = -1
+                
+        elif os.path.isdir(base_path):
+            print(f"The path '{base_path}' is a regular directory.")
+            return_value = 0
+            
+        elif os.path.isfile(base_path):
+            print(f"The path '{base_path}' is a regular file.")
+            return_value = 0
+            
+        else:
+            print(f"The path '{base_path}' does not exist.")
+            return_value = -1
+    
+        
 
 # Example Usage:
 if __name__ == '__main__':
@@ -62,19 +124,41 @@ if __name__ == '__main__':
     config_file = args.config
 
     # First instance creation
-    config1 = ConfigSingleton(config_file)
+    config = ConfigSingleton(config_file)
     print("First instance created.")
-    print("Seed:", config1.SEED)
-    print("Classification data path:", config1.CLASSIFICATION_DATA_BASE_PATH)
-    print("Target classes:", config1.TARGET_CLASS_LIST)
-    print("linear classifier type:", config1.LINEAR_CLASSIFIER_TYPE)
-    print("lambda aligns:", config1.LAMBDA_ALIGNS)
-    print("concept base path:", config1.CONCEPT_FOLDER_LIST)
-    print("Random folder path:", config1.RANDOM_FOLDER)
-    print("concept base path:", config1.CONCEPT_FOLDER_LIST)
-    print("Learning rate:", config1.LEARNING_RATE)
-    print("Batch size:", config1.BATCH_SIZE)    
+    print("Seed:", config.SEED)
+    print("Classification data path:", config.CLASSIFICATION_DATA_BASE_PATH)
+    print("Target classes:", config.TARGET_CLASS_LIST)
+    print("linear classifier type:", config.LINEAR_CLASSIFIER_TYPE)
+    print("lambda aligns:", config.LAMBDA_ALIGNS)
+    print("concept base path:", config.CONCEPT_FOLDER_LIST)
+    print("Random folder path:", config.RANDOM_FOLDER)
+    print("concept base path:", config.CONCEPT_FOLDER_LIST)
+    print("Learning rate:", config.LEARNING_RATE)
+    print("Batch size:", config.BATCH_SIZE)    
         
-    print("Number of classes:", config1.NUM_CLASSES)
+    print("Number of classes:", config.NUM_CLASSES)
 
     print("-" * 20)
+    notfound = 0
+    for concept_folder in config.CONCEPT_FOLDER_LIST:
+        results = config._verify_files_links(concept_folder)
+        if(results == -1):
+            print("Folder not found ",concept_folder)
+            notfound = 1
+    print(config.RANDOM_FOLDER)
+    
+    results = config._verify_files_links(config.RANDOM_FOLDER)
+    if(results == -1):
+        print("Folder not found ",random_folder)
+        notfound = 1
+    
+    base_path = config.CLASSIFICATION_DATA_BASE_PATH
+    for target in config.TARGET_CLASS_LIST:
+        folder_path = os.path.join(base_path,"train/"+ target)
+        results = config._verify_files_links(folder_path)
+        
+    base_path = config.CLASSIFICATION_DATA_BASE_PATH
+    for target in config.TARGET_CLASS_LIST:
+        folder_path = os.path.join(base_path,"valid/"+ target)
+        results = config._verify_files_links(folder_path)
