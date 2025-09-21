d945accacf03f1834368570a8423a67f5f8b3e1c
srikanthks.hsr <srikanthks.hsr@gmail.com>
2025-09-21 15:42:53 +0530

    .

---
+++ runrecalinnn_multiclass.sh
@@ -0,0 +1,5 @@
+python main.py --model_name vgg16  --model_path /home/srikanth/trained_models/pytorch/caltech --config config_cub_multiclass.yaml
+python main.py --model_name resnet50 --model_path /home/srikanth/trained_models/pytorch/caltech   --config config_cub_multiclass.yaml 
+python main.py --model_name inception_v3 --model_path /home/srikanth/trained_models/pytorch/caltech  --config config_cub_multiclass.yaml 
+python main.py --model_name mobilenet_v3_small --model_path /home/srikanth/trained_models/pytorch/caltech  --config config_cub_multiclass.yaml
+python main.py --model_name mobilenet_v3_large --model_path /home/srikanth/trained_models/pytorch/caltech  --config config_cub_multiclass.yaml
