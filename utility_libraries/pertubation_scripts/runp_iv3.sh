ea5df69377af75c5d321ac5d4cfa51c627c98481
srikanthks.hsr <srikanthks.hsr@gmail.com>
2026-02-22 18:12:11 +0530

    added pertubation scrits

--- utility_libraries/pertubation_scripts/runp_iv3.sh
+++ utility_libraries/pertubation_scripts/runp_iv3.sh
@@ -1,35 +1,57 @@
-#---------------------------------------IV3 -----------------------------------------------
-#BG
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c0_bg --layers_to_pertubate "maxpool2,Mixed_5d.branch3x3dbl_3.conv,Mixed_6e.branch_pool.conv,Mixed_7a.branch7x7x3_2.conv,Mixed_7c.branch1x1.conv"
+#!/bin/bash
+MODEL="inception_v3"
+# Define layer arrays for each artifact and class combination
+##Inception A Inception B Inception C Reduction A Reduction B Stem
+declare -A layers=(
+    [c0_bg]="Mixed_5d.branch3x3dbl_3.conv,Mixed_6e.branch_pool.conv,Mixed_7c.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_7a.branch7x7x3_2.conv,Conv2d_3b_1x1.conv"
+    [c0_coat]="Mixed_5b.branch1x1.conv,Mixed_6e.branch_pool.conv,Mixed_7c.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_7a.branch7x7x3_2.conv,Conv2d_3b_1x1.conv"
+    [c0_face]="Mixed_5c.branch1x1.conv,Mixed_6e.branch1x1.conv,Mixed_7b.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_7a.branch3x3_2.conv,Conv2d_3b_1x1.conv"
+    [c0_legs]="Mixed_5d.branch1x1.conv,Mixed_6e.branch1x1.conv,Mixed_7c.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_7a.branch3x3_2.conv,maxpool1"
 
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c1_bg --layers_to_pertubate "Conv2d_4a_3x3.conv,Mixed_5c.branch5x5_1.conv,Mixed_6e.branch7x7dbl_3.conv,Mixed_7a.branch7x7x3_2.conv,Mixed_7c.branch3x3dbl_3a.conv"
+    [c1_bg]="Mixed_5c.branch5x5_1.conv,Mixed_6e.branch7x7dbl_3.conv,Mixed_7c.branch1x1.conv,Mixed_7a.branch7x7x3_2.conv,Conv2d_4a_3x3.conv"
+    [c1_coat]="Mixed_5c.branch5x5_1.conv,Mixed_6e.branch7x7dbl_5.conv,Mixed_7c.branch1x1.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_7a.branch7x7x3_4.conv,Conv2d_3b_1x1.conv"
+    [c1_face]="Mixed_5c.branch5x5_2.conv,Mixed_6e.branch7x7dbl_5.conv,Mixed_7b.branch1x1.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_7a.branch7x7x3_4.conv,Conv2d_4a_3x3.conv"
+    [c1_legs]="Mixed_5c.branch5x5_1.conv,Mixed_6e.branch7x7dbl_5.conv,Mixed_7b.branch3x3dbl_3b.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_7a.branch7x7x3_4.conv,Conv2d_4a_3x3.conv"
 
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c2_bg --layers_to_pertubate "Conv2d_2a_3x3.conv,Mixed_5c.branch_pool.conv,Mixed_6a.branch3x3dbl_1.conv,Mixed_6d.branch7x7dbl_5.conv,Mixed_7a.branch7x7x3_2.conv,Mixed_7c.branch3x3_2a.conv"
+    [c2_bg]="Mixed_5c.branch_pool.conv,Mixed_6d.branch7x7dbl_5.conv,Mixed_7c.branch3x3_2a.conv,Mixed_6a.branch3x3dbl_1.conv,Mixed_7a.branch7x7x3_2.conv,Conv2d_2a_3x3.conv"
+    [c2_coat]="Mixed_5d.branch3x3dbl_3.conv,Mixed_6e.branch_pool.conv,Mixed_7c.branch1x1.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_7a.branch7x7x3_4.conv,maxpool2"
+    [c2_face]="Mixed_5c.branch1x1.conv,Mixed_6e.branch1x1.conv,Mixed_7b.branch1x1.conv,Mixed_6a.branch3x3.conv,maxpool2"
+    [c2_legs]="Mixed_5c.branch_pool.conv,Mixed_6e.branch1x1.conv,Mixed_7b.branch3x3_1.conv,Mixed_6a.branch3x3dbl_3.conv,Conv2d_2b_3x3.conv"
 
-#Coat
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c0_coat --layers_to_pertubate "Conv2d_3b_1x1.conv,Mixed_5b.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_6e.branch_pool.conv,Mixed_7a.branch7x7x3_2.conv,Mixed_7c.branch3x3_1.conv"
+)
 
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c1_coat --layers_to_pertubate "Conv2d_3b_1x1.conv,Mixed_5c.branch5x5_1.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_6e.branch7x7dbl_5.conv,Mixed_7a.branch7x7x3_4.conv,Mixed_7c.branch3x3_2a.conv"
-
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c2_coat --layers_to_pertubate "maxpool2,Mixed_5d.branch3x3dbl_3.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_6e.branch1x1.conv,Mixed_7a.branch7x7x3_4.conv,Mixed_7c.branch3x3_2b.conv"
-
-#Face
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c0_face --layers_to_pertubate "Conv2d_3b_1x1.conv,Mixed_5c.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_6e.branch1x1.conv,Mixed_7a.branch3x3_2.conv,Mixed_7c.branch1x1.conv"
-
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c1_face --layers_to_pertubate "Conv2d_4a_3x3.conv,Mixed_5c.branch5x5_2.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_6e.branch7x7dbl_5.conv,Mixed_7a.branch7x7x3_4.conv,Mixed_7c.branch1x1.conv"
-
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c2_face --layers_to_pertubate "maxpool2,Mixed_5c.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_6d.branch1x1.conv,Mixed_7b.branch3x3_2b.conv"
-
-#Legs
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c0_legs --layers_to_pertubate "maxpool1,Mixed_5d.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_6d.branch1x1.conv,Mixed_7a.branch3x3_2.conv,Mixed_7c.branch3x3dbl_3a.conv"
-
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c1_legs --layers_to_pertubate "Conv2d_4a_3x3.conv,Mixed_5c.branch5x5_1.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_6e.branch7x7dbl_5.conv,Mixed_7a.branch3x3_2.conv,Mixed_7c.branch3x3dbl_3b.conv"
-
-python pertubate_neurons.py --model_name inception_v3 --config layer_config.yaml --saveas c2_legs --layers_to_pertubate "Conv2d_2b_3x3.conv,Mixed_5c.branch_pool.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_6e.branch1x1.conv,Mixed_7b.branch3x3_2b.conv"
+# Iterate through each configuration
+for config in "${!layers[@]}"; do
+    python pertubate_neurons.py \
+        --model_name "$MODEL" \
+        --config layer_config.yaml \
+        --saveas "$config" \
+        --layers_to_pertubate "${layers[$config]}"
+done
 
 
-mkdir -p /mnt/sdc/sensitivity_analysis_paper/perturbation/iv3_new
-mv *.xlsx /mnt/sdc/sensitivity_analysis_paper/perturbation/iv3_new
-mv pert*.log /mnt/sdc/sensitivity_analysis_paper/perturbation/iv3_new
+declare -A layers_negative=(
+    [c0_bg_negative]="Mixed_5b.branch_pool.conv,Mixed_6b.branch7x7dbl_5.conv,Mixed_7b.branch1x1.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_7a.branch3x3_2.conv,Conv2d_4a_3x3.conv"
+    [c0_coat_negative]="Mixed_5b.branch_pool.conv,Mixed_6b.branch7x7dbl_1.conv,Mixed_7b.branch3x3dbl_1.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_7a.branch3x3_1.conv,Conv2d_2a_3x3.conv"
+    [c0_face_negative]="Mixed_5b.branch_pool.conv,Mixed_6d.branch7x7dbl_1.conv,Mixed_7b.branch3x3dbl_1.conv,Mixed_6a.branch3x3dbl_1.conv,Mixed_7a.branch7x7x3_1.conv,Conv2d_1a_3x3.conv"
+    [c0_legs_negative]="Mixed_5b.branch_pool.conv,Mixed_6d.branch7x7_3.conv,Mixed_7b.branch3x3dbl_1.conv,Mixed_6a.branch3x3dbl_3.conv,Conv2d_1a_3x3.conv"
 
-#---------------------------------------RNet 50 -----------------------------------------------
+    [c1_bg_negative]="Mixed_5b.branch1x1.conv,Mixed_6b.branch1x1.conv,Mixed_7b.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_7a.branch7x7x3_1.conv,Conv2d_2b_3x3.conv"
+    [c1_coat_negative]="Mixed_5c.branch1x1.conv,Mixed_6b.branch1x1.conv,Mixed_7b.branch3x3dbl_2.conv,Mixed_6a.branch3x3.conv,Mixed_7a.branch3x3_2.conv,Conv2d_2b_3x3.conv"
+    [c1_face_negative]="Mixed_5c.branch1x1.conv,Mixed_6b.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_7a.branch7x7x3_1.conv,Conv2d_2b_3x3.conv"
+    [c1_legs_negative]="Mixed_5c.branch1x1.conv,Mixed_6b.branch1x1.conv,Mixed_6a.branch3x3.conv,Mixed_7a.branch7x7x3_1.conv,Conv2d_2b_3x3.conv"
+
+    [c2_bg_negative]="Mixed_5c.branch1x1.conv,Mixed_6b.branch1x1.conv,Mixed_7b.branch1x1.conv,Mixed_6a.branch3x3dbl_3.conv,Mixed_7a.branch3x3_1.conv,maxpool1"
+    [c2_coat_negative]="Mixed_5b.branch_pool.conv,Mixed_6e.branch7x7_1.conv,Mixed_7b.branch3x3dbl_1.conv,Mixed_6a.branch3x3dbl_2.conv,Mixed_7a.branch3x3_1.conv,Conv2d_3b_1x1.conv"
+    [c2_face_negative]="Mixed_5c.branch5x5_2.conv,Mixed_6c.branch7x7dbl_2.conv,Mixed_7b.branch3x3dbl_1.conv,Mixed_6a.branch3x3dbl_2.conv,Mixed_7a.branch3x3_1.conv,maxpool1"
+    [c2_legs_negative]="Mixed_5c.branch3x3dbl_3.conv,Mixed_6c.branch7x7_1.conv,Mixed_7b.branch3x3dbl_1.conv,Mixed_6a.branch3x3dbl_2.conv,Mixed_7a.branch3x3_1.conv,maxpool1"
+)
+
+# Iterate through each configuration
+for config in "${!layers_negative[@]}"; do
+    python pertubate_neurons.py \
+        --model_name "$MODEL" \
+        --config layer_config.yaml \
+        --saveas "$config" \
+        --layers_to_pertubate "${layers_negative[$config]}"
+done
--- utility_libraries/pertubation_scripts/runp_rnet.sh
+++ utility_libraries/pertubation_scripts/runp_rnet.sh
@@ -1,26 +1,62 @@
-#---------------------------------------RNet 50 -----------------------------------------------
-#BG
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c0_bg --layers_to_pertubate "layer1.2.conv3,layer2.0.conv3,layer3.0.conv1,layer4.2.conv3"
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c1_bg --layers_to_pertubate "maxpool,layer1.0.conv1,layer2.0.conv2,layer3.5.conv2,layer4.2.conv3"
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c2_bg --layers_to_pertubate "layer3.5.conv2,layer4.2.conv3"
+#!/bin/bash
+# Define layer arrays for each artifact and class combination
+declare -A layers=(
+    [c0_bg]="layer1.2.conv3,layer2.0.conv3,layer3.0.conv1,layer4.2.conv3"
+    [c0_coat]="layer1.2.conv3,layer2.0.conv2,layer3.0.downsample.0,layer4.2.conv3"
+    [c0_face]="maxpool,layer1.2.conv3,layer2.0.conv2,layer3.0.conv2,layer4.2.conv3"
+    [c0_legs]="layer1.2.conv3,layer2.0.conv2,layer3.0.downsample.0,layer4.0.conv2"
 
-#Coat
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c0_coat --layers_to_pertubate "layer1.2.conv3,layer2.0.conv2,layer3.0.downsample.0,layer4.2.conv3"
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c1_coat --layers_to_pertubate "maxpool,layer1.0.conv1,layer2.0.conv2,layer3.0.downsample.0,layer4.2.conv3"
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c2_coat --layers_to_pertubate "layer1.0.downsample.0,layer2.0.conv3,layer3.5.conv3,layer4.2.conv3"
+    [c1_bg]="maxpool,layer1.0.conv1,layer2.0.downsample.0,layer3.0.downsample.0,layer4.1.conv3"
+    [c1_coat]="maxpool,layer1.0.conv1,layer2.0.downsample.0,layer3.0.downsample.0,layer4.2.conv3"
+    [c1_face]="maxpool,layer1.0.conv1,layer2.0.downsample.0,layer3.0.downsample.0,layer4.1.conv3"
+    [c1_legs]="maxpool,layer1.0.conv1,layer2.0.downsample.0,layer3.0.downsample.0,layer4.1.conv3"
 
-#Face
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c0_face --layers_to_pertubate "layer1.0.conv3,layer2.0.conv2,layer3.0.conv2,layer4.2.conv3" 
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c1_face --layers_to_pertubate "maxpool,layer1.0.conv1,layer2.0.conv2,layer3.0.downsample.0,layer4.2.conv3"
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c2_face --layers_to_pertubate "layer3.0.conv2,layer4.2.conv2"
+    [c2_bg]="layer1.2.conv1,layer3.5.conv2,layer4.2.conv3"
+    [c2_coat]="layer1.0.downsample.0,layer2.0.conv3,layer3.5.conv3,layer4.1.conv3"
+    [c2_face]="layer3.0.conv2,layer4.0.conv2"
+    [c2_legs]=" layer4.0.conv3"
 
-#Legs
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c0_legs --layers_to_pertubate "layer1.2.conv3,layer2.3.conv3,layer3.0.conv1"
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c1_legs --layers_to_pertubate "maxpool,layer1.1.conv1,layer2.0.downsample.0,layer3.0.downsample.0,layer4.2.conv3"
-python pertubate_neurons.py --model_name resnet50 --config layer_config.yaml --saveas c2_legs --layers_to_pertubate "layer4.0.conv2"
+)
+
+# Iterate through each configuration
+for config in "${!layers[@]}"; do
+    python pertubate_neurons.py \
+        --model_name resnet50 \
+        --config layer_config.yaml \
+        --saveas "$config" \
+        --layers_to_pertubate "${layers[$config]}"
+done
 
 
-#mv resnet50*.xlsx /mnt/sdc/sensitivity_analysis_paper/perturbation/rnet50
-#mv pert*.log /mnt/sdc/sensitivity_analysis_paper/perturbation/rnet50
+declare -A layers_negative=(
+    [c0_bg_negative]="maxpool,layer1.0.conv1,layer2.1.conv2,layer3.1.conv1,layer4.2.conv1"
+    [c0_coat_negative]="maxpool,layer1.1.conv2,layer2.1.conv3,layer3.1.conv1,layer4.2.conv1"
+    [c0_face_negative]="conv1,layer1.2.conv1,layer2.1.conv3,layer3.1.conv1,layer4.1.conv2"
+    [c0_legs_negative]="maxpool,layer1.1.conv1,layer2.1.conv3,layer3.0.conv3,layer4.0.downsample.0"
+
+    [c1_bg_negative]="conv1,layer1.2.conv1,layer2.3.conv3,layer3.2.conv1,layer4.1.conv2"
+    [c1_coat_negative]="conv1,layer1.2.conv1,layer2.3.conv3,layer3.0.conv1,layer4.1.conv2"
+    [c1_face_negative]="conv1,layer1.2.conv1,layer2.1.conv1,layer3.2.conv1"
+    [c1_legs_negative]="conv1,layer1.2.conv1,layer2.0.conv1,layer3.2.conv1"
+
+    [c2_bg_negative]="maxpool,layer1.0.conv1,layer2.0.conv1,layer3.0.downsample.0,layer4.0.downsample.0"
+    [c2_coat_negative]="maxpool,layer1.0.conv1,layer2.1.conv2,layer3.0.downsample.0"
+    [c2_face_negative]="maxpool,layer1.0.conv1,layer2.0.conv2,layer3.0.conv1,layer4.2.conv1"
+    [c2_legs_negative]="maxpool,layer1.0.conv1,layer2.0.conv1,layer3.0.conv1,layer4.1.conv2"
+
+)
+
+# Iterate through each configuration
+for config in "${!layers_negative[@]}"; do
+    python pertubate_neurons.py \
+        --model_name resnet50 \
+        --config layer_config.yaml \
+        --saveas "$config" \
+        --layers_to_pertubate "${layers_negative[$config]}"
+done
+
+
+mv resnet50*.xlsx /mnt/sdc/sensitivity_analysis_paper/perturbation/rnet50
+mv pert*.log /mnt/sdc/sensitivity_analysis_paper/perturbation/rnet50
 
 #---------------------------------------RNet 50 -----------------------------------------------
